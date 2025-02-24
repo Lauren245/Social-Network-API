fe586bbba2ff2a2035ce4ad7c3dea041b0d51ef0
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-02-23 16:19:29 -0800

    worked on Thought Schema.

--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -2,12 +2,59 @@ import { Schema, Types, model, type Document } from "mongoose";
 import dayjs from "dayjs";
 
 interface IReaction extends Document {
-    reactionId: Schema.Types.ObjectId
-}
+    reactionId: Schema.Types.ObjectId,
+    reactionBody: string,
+    createdAt: Date,
+    username: string
+};
 
 interface IThought extends Document {
     thoughtText: string,
     createdAt: Date,
-    username: string, /*??? Is this not connected to the username property of a User?*/
+    username: string, 
+    reactions: Schema.Types.ObjectId[]
+};
 
-}
+const reactionSchema = new Schema<IReaction>(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: [true, "No reaction body provided"],
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: () => dayjs().toDate(),
+            immutable: true //prevents modification after creation
+        }
+    }
+);
+
+const thoughtSchema = new Schema<IThought>(
+    {
+        thoughtText: {
+            type: String,
+            required: [true, "Thought text is required."],
+            validate: {
+                validator: function (value: string) {
+                    return value.length >= 1 && value.length <= 280;
+                },
+                message: "Thought text must be between 1 and 280 characters."
+            }
+        },
+        createdAt: {
+            type: Date,
+            default: () => dayjs().toDate(),
+            immutable: true //prevents modification after creation
+        },
+        username: {
+            type: String,
+            required: [true, "A username is required."]
+        },
+        reactions: [reactionSchema]
+    }
+);
